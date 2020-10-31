4904f34d0d1d15e4affd18ee90f34ddd69100856
jaebokcom <jaebokcom@118.221.203.207>
2020-10-31 22:24:22 +0900

    레벨2 수박수박수 첫번째커밋

---
+++ src/lv1/WaterMelon.java
@@ -0,0 +1,26 @@
+package lv1;
+
+public class WaterMelon {
+	 public String watermelon(int n) {
+	        String answer = "";
+	        
+	        for(int i=0; i<n; i++) {
+				if(i%2==0) {
+					answer +="수";
+				}else if(i%2!=0) {
+					answer +="박";
+				}
+			}
+	        
+	        return answer;
+	    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WaterMelon wm = new WaterMelon();
+		System.out.println("n이 3인 경우: " + wm.watermelon(3));
+        System.out.println("n이 4인 경우: " + wm.watermelon(4));
+
+	}
+
+}
