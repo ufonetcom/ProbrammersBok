4569125ed1a200373fd8e6fd1f7bd53e77ba1252
jaebokcom <ufozx@naver.com>
2020-11-18 12:47:37 +0900

    lv1 예산 (문제 해석 능력을 키우자... 문제를 잘못 이해하여 오래걸린 문제!)(항상 꼼꼼히 읽기)

---
+++ src/lv1/Yesan.java
@@ -0,0 +1,34 @@
+package lv1;
+
+import java.util.Arrays;
+
+public class Yesan {
+   public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum=0;
+        Arrays.sort(d);
+        for(int i=0; i<d.length; i++) {
+           sum += d[i];
+           if(sum>budget) {
+              answer = i;
+              break;
+           }
+        }
+        if(sum<=budget) {
+           answer = d.length;
+        }
+        return answer;
+    }
+   
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+      Yesan ye = new Yesan();
+//      int[] d = {1,3,2,5,4};
+      int[] d = {2,2,3,3};
+      
+//      int budget = 9;
+      int budget = 10;
+      System.out.println(ye.solution(d, budget));
+   }
+
+}
