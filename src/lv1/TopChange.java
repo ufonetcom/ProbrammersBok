d60b3df16f514fd359cae346d9daef4295e7e374
jaebokcom <ufozx@naver.com>
2020-11-21 01:02:49 +0900

    lv1 자연수 뒤집어 배열로 만들기 (형변환 아주아주 조심... 타입별 범위로 인한 데이터 손실때문에 테스트케이스 3개가 오류낫다..) --> long 타입의 값만 int로 형변환 시키고 10으로 나누었기 때문에 이미 깨진값을 나눈 꼴.. 조심하자 형변환!

---
+++ src/lv1/TopChange.java
@@ -0,0 +1,33 @@
+package lv1;
+
+import java.util.Arrays;
+
+public class TopChange {
+	public int[] solution(long n) {
+        int[] answer = {};
+        long copy = n;
+        
+        
+//        배열 answer의 크기를 정해주는 함수
+        int k=0;
+        while(n!=0) {
+        	k++;
+        	n=n/10;
+        }
+        answer = new int[k];
+        
+        for(int i=0; i<answer.length; i++) {
+        	answer[i]= (int)(copy%10); //매우매우 중요.. long타입의 값을 (int)copy%10 이런식으로 형변환하면 데이터 손실일어남. 
+        	copy = copy/10;
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TopChange tc = new TopChange();
+		System.out.println(Arrays.toString(tc.solution(9879879879L)));
+	}
+
+}
