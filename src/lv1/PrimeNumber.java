043c0645e52a73430ea8b6921e4a55c49969fe11
jaebokcom <ufozx@naver.com>
2020-11-30 00:11:42 +0900

    lv1. 소수찾기(정말 오래걸렸던 문제 효율성 확인하기!!!!)

--- src/lv1/PrimeNumber.java
+++ src/lv1/PrimeNumber.java
@@ -4,7 +4,8 @@ public class PrimeNumber {
 	   public int solution(int n) {
 	        int answer = 0;
 	        boolean[] check = new boolean[n+1]; //n까지의 수를 boolean배열에 담는다.(index값이 곧 해당 수)
-//	      i가 1씩 커질때마다 i의 배수를 지워나가는 식으로 n이 커질때 연산 횟수를 크게 줄여나갈 수 있다.
+	        //i가 1씩 커질때마다 i의 배수를 지워나가는 식으로 n이 커질때 연산 횟수를 크게 줄여나갈 수 있다.
+	        
 	        for(int i=2; i<=n; i++) {
 	           if(check[i]==false) { //boolean 초기값은 false
 	              answer++;
