10d5460baadf009dfe409643ff4e76af79520454
jaebokcom <ufozx@naver.com>
2021-01-07 00:07:06 +0900

    lv1.  콜라츠 추측 문제풀이!! (정수형 오버플로우 생각하기 알아보기)

---
+++ src/lv1/Collatz.java
@@ -0,0 +1,32 @@
+package lv1;
+
+public class Collatz {
+	public int solution(int num) {
+        int answer = 0;
+        
+        while(num!=1) {
+        	if(num%2==0) {
+        		num = num / 2;
+        	}
+        	else if(num%2==1){
+        		num = (num*3) + 1;
+        	}
+        	answer++;
+        	
+        	System.out.println(answer + "  " + num);
+        	if(answer==500) {
+        		return -1;
+        	}
+        }
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Collatz col = new Collatz();
+		System.out.println(col.solution(626331));
+
+	}
+
+}
