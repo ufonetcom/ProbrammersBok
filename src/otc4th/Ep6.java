b390dc24881b1845730b899eb0cd057475bf8633
ufonetcom <ufozx@naver.com>
2021-11-09 21:20:45 +0900

    otc4th ready for test - ep6 time setting, substring, create method(리팩토리)

--- src/otc4th/Ep6.java
+++ src/otc4th/Ep6.java
@@ -1,7 +1,7 @@
 package otc4th;
 public class Ep6 {
 	public String solution(double time, String[][] plans) {
-		String answer = "";
+		String answer = "호치민";
 
 		double gotoWorkTimeOnMonday = 13;
 		double offWorkTimeOnMonday = 18;
@@ -9,7 +9,7 @@ public class Ep6 {
 		double offWorkTimeOnFriday = 18;
 
 		for (int i = 0; i < plans.length; i++) {
-			double realT = (int)time*60; // 0.5당 30분,,,휴가시간
+			double realT = time; // 0.5당 30분,,,휴가시간
 			String loc = plans[i][0]; //위치 ex)"홍콩"
 			String startT = plans[i][1]; //출발 시간 ex)11PM
 			String arriveT = plans[i][2]; //도착 시간 ex)9PM
@@ -17,11 +17,13 @@ public class Ep6 {
 			int startTR = reversToAmPm(subStringTime(startT),subStringAmpm(startT)); //ex)24시 기준표현식, 21(시)
 			int arriveTR = reversToAmPm(subStringTime(arriveT), subStringAmpm(arriveT));
 
-			if(startTR<offWorkTimeOnFriday){ //출발시간이 금요일 퇴근시간 전 일때
-				realT -= (offWorkTimeOnFriday - startTR)*60;
+			if(startTR<offWorkTimeOnFriday){ //출발시간이 금요일 퇴근시간 전 일때 15 / 18
+				if(startTR<gotoWorkTimeOnFriday) realT -= (offWorkTimeOnFriday - gotoWorkTimeOnFriday); // 15/ 9.5
+				else realT -= (offWorkTimeOnFriday - startTR);
 			}
-			if(arriveTR>gotoWorkTimeOnMonday){ //도착시간이 월요일 출근시간을 넘을때
-				realT -= (arriveTR-gotoWorkTimeOnFriday)*60;
+			if(arriveTR>gotoWorkTimeOnMonday){ //도착시간이 월요일 출근시간을 넘을때  16 / 13
+				if(arriveTR>offWorkTimeOnMonday) realT -= (offWorkTimeOnMonday-gotoWorkTimeOnMonday);
+				else realT -= (arriveTR-gotoWorkTimeOnMonday);
 			}
 			if(realT>0) answer = loc;
 		}
@@ -49,8 +51,8 @@ public class Ep6 {
 		Ep6 sol = new Ep6();
 
 		//입력값
-		double time = 3.5;
-		String[][] plans = {{"홍콩","6PM","5PM"},{"엘에이","11PM","9AM"}};
+		double time = 7;
+		String[][] plans = {{"홍콩","9AM","7PM"},{"엘에이","3PM","4PM"}};
 
 		System.out.print(sol.solution(time, plans));
 	}
